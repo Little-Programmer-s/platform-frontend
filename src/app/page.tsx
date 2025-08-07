28700c27b764dacd40a34745c41fe1cce39f2e53
Talkhestani <talkhestani@outlook.com>
2025-08-07 16:51:12 +0330

    📦 NEW: technical section complete grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,14 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      {/* Technical Section */}
+      <div className='c-space container mx-auto max-w-7xl grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[14rem] mt-12'>
+        <div className='grid-default-color grid-1'></div>
+        <div className='grid-default-color grid-2'></div>
+        <div className='grid-default-color grid-3'></div>
+        <div className='grid-black-color grid-4'></div>
+        <div className='grid-special-color grid-6'></div>
+        <div className='grid-default-color grid-5'></div>
+      </div>
       {/* Project Section */}
       {/* Testimonial Section */}
       {/* Billing Section */}
