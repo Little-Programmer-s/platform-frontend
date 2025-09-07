dc7dc8c03b77e4bdb459d0848ce5696470bfe5e9
Talkhestani <talkhestani@outlook.com>
2025-09-07 17:36:32 +0330

    📦 NEW: complete project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,24 @@
 import Hero from "@/components/pages/hero-section";
+import Team from "@/components/pages/team-section";
 import Tech from "@/components/pages/tech-section";
 import Testimonial from "@/components/pages/testimonial-section";
+import Footer from "@/components/partials/footer";
 import Navbar from "@/components/partials/navbar";
 
-import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
 
 
 
-const data = [
-  {
-    category: "Unknown",
-    title: "Habibetoon",
-    src: "/assets/images/Habibetoon.jpg",
-    content: "",
-  },
-  {
-    category: "Frontend Developer",
-    title: "Amirhossein Amiri",
-    src: "/assets/images/Amiri.jpg",
-    content: "",
-  },
-  {
-    category: "Backend Developer",
-    title: "Mohammad Hossein Alidadi Talkhestani",
-    src: "/assets/images/talkhestani.jpg",
-    content: "",
-  },
-];
+
 
 export default function Home() {
-  const cards = data.map((card, index) => (
-    <Card key={card.src} card={card} index={index} />
-  ));
-
-
   return (
     <>
       <Navbar />
       <Hero />
       <Tech />
       <Testimonial />
-      <div className="container max-w-7xl mx-auto c-space mt-32">
-          <h1 className="text-center text-5xl font-bold mb-4">Team  <span className="text-lavender">members</span></h1>
-          <Carousel items={cards} />
-      </div>
-      {/* Footer */}
+      <Team />
+      <Footer />
     </>
   );
 }
